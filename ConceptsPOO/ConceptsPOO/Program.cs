d415dc0881ebbc7e71046a9536d83da6989077af
administrador <pavas0921@gmail.com>
2022-04-23 17:38:45 -0500

    Finish date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using ConceptsPOO;
+
+
+try
+{
+    Console.WriteLine(new Date(2024, 2, 29));
+    Console.WriteLine(new Date(2023, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 31));
+}
+catch (Exception error)
+{
+
+    Console.WriteLine(error.Message);
+}
+
+
+
---
+++ ConceptsPOO/ConceptsPOO/dayException.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace ConceptsPOO
+{
+    [Serializable]
+    internal class dayException : Exception
+    {
+        public dayException()
+        {
+        }
+
+        public dayException(string? message) : base(message)
+        {
+        }
+
+        public dayException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected dayException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
